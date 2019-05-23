a2f0a6ccafc30632a1c734c5f65a5b2e8028ccd5
Supayut Raksuk <nookskill@hotmail.com>
2019-05-24 00:47:38 +0700

    add axios and music.json

--- src/views/Music.vue
+++ src/views/Music.vue
@@ -1,5 +1,48 @@
 <template>
   <div class="music">
-    <h1>Comming soon.</h1>
+    <h1>My song</h1>
+    <div class="box" v-for="item in data">
+
+      <b>title: {{item.title}}</b><br>
+      <b>artist: {{item.artist}}</b><br>
+      <b>writer: {{item.writer}}</b><br>
+      <b>soundcloud: {{item.soundcloud}}</b><br>
+      <b>itunes: {{item.itunes}}</b><br>
+      <b>applemusic: {{item.applemusic}}</b><br>
+      <b>spotify: {{item.spotify}}</b><br>
+      <b>youtube: {{item.youtube}}</b><br>
+      <b>date: {{item.date}}</b><br>
+      <b>description: {{item.description}}</b><br>
+    </div>
+    <div class="empty-box"></div>
   </div>
 </template>
+
+<script>
+  import axios from 'axios';
+  export default {
+    data() {
+      return {
+        data: [],
+      }
+    },
+    mounted () {
+      axios
+        .get('/music.json')
+        .then(response => (this.data = response.data.songs))
+    }
+  }
+</script>
+
+<style>
+  .box {
+    text-align: left;
+    border: #42b983;
+    border-style: dotted;
+    padding: 15px;
+    margin-bottom: 15px;
+  }
+  .empty-box {
+    margin-top: 100px;
+  }
+</style>
