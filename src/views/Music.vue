c7a581b030d4bb52a2dabfc5aa3a59f08afc4698
Supayut Raksuk <nookskill@hotmail.com>
2019-05-28 22:37:39 +0700

    update music page

--- src/views/Music.vue
+++ src/views/Music.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="music">
     <h1>My song</h1>
-    <div class="box" v-for="item in data">
-
-      <b>title: {{item.title}}</b><br>
-      <b>artist: {{item.artist}}</b><br>
-      <b>writer: {{item.writer}}</b><br>
-      <b>soundcloud: {{item.soundcloud}}</b><br>
-      <b>itunes: {{item.itunes}}</b><br>
-      <b>applemusic: {{item.applemusic}}</b><br>
-      <b>spotify: {{item.spotify}}</b><br>
-      <b>youtube: {{item.youtube}}</b><br>
-      <b>date: {{item.date}}</b><br>
-      <b>description: {{item.description}}</b><br>
+    <div v-for="item in data">
+      <div class="box-left"></div>
+      <div class="box-main">
+        <div>title: {{item.title}}</div>
+        <div>artist: {{item.artist}}</div>
+        <div>writer: {{item.writer}}</div>
+        <div>soundcloud: {{item.soundcloud}}</div>
+        <div v-if="item.itunes">itunes: <a v-bind:href="item.itunes">Click here</a></div>
+        <div v-else>itunes: - </div>
+        <div v-if="item.applemusic">applemusic: <a v-bind:href="item.applemusic">Click here</a></div>
+        <div v-else>applemusic: - </div>
+        <div v-if="item.spotify">spotify: <a v-bind:href="item.spotify">Click here</a></div>
+        <div v-else>spotify: - </div>
+        <div v-if="item.youtube">youtube: <a v-bind:href="item.youtube">Click here</a></div>
+        <div v-else>youtube: - </div>
+        <div>date: {{item.date}}</div>
+        <div>description: {{item.description}}</div>
+      </div>
+      <div class="box-right"></div>
     </div>
     <div class="empty-box"></div>
   </div>
@@ -35,14 +42,44 @@
 </script>
 
 <style>
-  .box {
+  .box-left {
+    width: 20%;
+  }
+
+  .box-main {
     text-align: left;
+    font-weight: bold;
+
     border: #42b983;
     border-style: dotted;
     padding: 15px;
     margin-bottom: 15px;
   }
+
+  .box-right {
+    width: 20%;
+  }
+
   .empty-box {
     margin-top: 100px;
+  }
+
+  a {
+    color: #c5c6c7;
+  }
+
+  @media only screen and (max-width: 540px) {
+    .box-main {
+      text-align: left;
+      font-weight: bold;
+
+      border: #42b983;
+      border-style: dotted;
+      padding: 15px;
+      margin-bottom: 15px;
+    }
+    .empty-box {
+      margin-top: 100px;
+    }
   }
 </style>
